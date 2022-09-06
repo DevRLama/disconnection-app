b6f11620c723c9b45a553385b3c288a8b82c7c42
amitaeit <amitaeit@gmail.com>
2022-09-06 13:36:29 +0530

    User Changes

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,30 @@
 const user = require('../model/User');
 const userMapping = require('../model/UserMapping');
 const express = require('express');
+const User = require('../model/User');
 
 const router = express.Router();
 
-router.post('/login',(req,resp)=>{
+router.get('/getotp',async(req,resp)=>{
+    const userId = req.query.mobileno;
+    console.log(userId);
+    if(userId){
+        const user = await User.findOne({userId});
+        if(user){
+            user.otp="1234";
+            resp.send({respCode:1,user:user});
+        }
+        else{
+            resp.send({respCode:3,respMsg:"User with Mobile No. does not exist"});
+        }
+    }
+    else{
+        resp.send({respCode:2,respMsg:"Mobile No empty"})
+    }
+    
+})
+
+router.post('/verifyotp',(req,resp)=>{
     
 })
 
