b5552cc574f7d488effec4a4b108bffdb8fb2004
amitaeit <amitaeit@gmail.com>
2022-09-08 14:58:25 +0530

    added assign task api

--- backend/routes/dc.js
+++ backend/routes/dc.js
@@ -1,6 +1,7 @@
 const Disconnection = require('../model/Disconnection');
-const DisconnectionAssignment = require('../model/DisconnectionAssignment');
 const express = require('express');
+const { body, validationResult } = require('express-validator');
+const { exists } = require('../model/Disconnection');
 
 const router = express.Router();
 
@@ -15,6 +16,22 @@ router.get('/getdc', async (req, resp) => {
     else{
         resp.send({respCode:2,respMsg:"count not set"})
     }
+})
+
+router.post('/assigndc',[ 
+    body().isArray(),
+    body('*.accountId', 'accountId field is required').exists( {checkFalsy: true}),
+    body('*.jeId', 'JE field is required').exists({checkFalsy: true}),
+    body('*.linemanId', 'lineman Id field is required').exists({checkFalsy: true})], async (req,resp)=>{
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return resp.send({ respCode: 2, respMsg: errors.array() })
+        }
+        
+        console.log(req.body);
+
+
+
 })
 
 module.exports = router;
